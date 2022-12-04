4ee74c4d38337463ff2af5c80a9a095ded70b6f0
Yxmaxy <vrecer.marko@gmail.com>
2022-12-04 16:54:12 +0100

    Finished CalendarTime

--- frontend/src/components/CalendarTime.vue
+++ frontend/src/components/CalendarTime.vue
@@ -1,26 +1,31 @@
 <template>
+    <div>
+        <button @click="selectedWeek--">Prev</button>
+        <button @click="selectedWeek++">Next</button>
+    </div>
     <div class="calendar-component">
-        <div
-            v-for="day in days"
-            :class="{
-                'in-range': day.isInRange,
-                'available': day.isAvailable && !selectUnavailable,
-                'unavailable': day.isInRange && selectUnavailable && !day.isAvailable,
-            }"
-            @mousedown="(event) => onMouseDown(event, day)"
-            @mouseenter="(event) => onMouseEnter(event, day)"
-            @mouseup="onMouseUp"
-            @mouseleave="(event) => onMouseLeave(event, day)"
-        >
-            {{day.display}}
-        </div>
+        <template v-for="(day, index) in days">
+            <div
+                v-if="(index >= selectedWeek * 24 * 7 && index < (1 + selectedWeek) * 24 * 7)"
+                :class="{
+                    'in-range': day.isInRange,
+                    'available': day.isAvailable && !selectUnavailable,
+                    'unavailable': day.isInRange && selectUnavailable && !day.isAvailable,
+                }"
+                @mousedown="(event) => onMouseDown(event, day)"
+                @mouseenter="(event) => onMouseEnter(event, day)"
+                @mouseup="onMouseUp"
+                @mouseleave="(event) => onMouseLeave(event, day)"
+            >
+                {{day.display}}
+            </div>
+        </template>
     </div>
 </template>
 
 <script lang="ts">
 import { formatDateHourDayMonth } from "../common/helpers";
 import { IDateRange, ICalendarDate } from '../common/interfaces';
-const longpressTimeout = 475;
 
 export default {
     name: "calendar",
@@ -42,6 +47,7 @@ export default {
             days: [] as ICalendarDate[],
             touchStart: undefined as ICalendarDate|undefined,
             timeoutObj: undefined as number|undefined,
+            selectedWeek: 0,
         }
     },
     methods: {
@@ -56,13 +62,14 @@ export default {
             const d = new Date(date);  // copy date
             const dayOfWeek = (end ? 6 - this.getDayMonToSun(d) : this.getDayMonToSun(d)) + buffer;
             return end ?
-                new Date(d.setDate(d.getDate() + dayOfWeek)) :
+                new Date(new Date(d.setDate(d.getDate() + dayOfWeek)).setHours(23)) :
                 new Date(d.setDate(d.getDate() - dayOfWeek));
             
         },
         // calculate dates to display on calendar
         calculateShownDates() {
             const range = this.dateRange as IDateRange;
+            range.to.setHours(24);  // set to full day
             const from = this.getBufferedDate(range.from, 0);
             const to = this.getBufferedDate(range.to, 0, true);
 
@@ -70,7 +77,7 @@ export default {
                 this.days.push({
                     display: formatDateHourDayMonth(d),
                     date: new Date(d),
-                    isInRange: d >= range.from && d <= range.to,
+                    isInRange: d >= range.from && d < range.to,
                 });
             }
         },
@@ -110,11 +117,11 @@ export default {
 </script>
 <style lang="scss" scoped>
 .calendar-component {
-
     box-sizing: border-box;
     flex: 1;
     display: grid;
-    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: repeat(24, 1fr);
+    grid-auto-flow: column;
     gap: 2px;
 
     // td
