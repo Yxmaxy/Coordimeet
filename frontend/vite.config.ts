c0b05e2c62e1a54167c82460c516a2cd067b6ac6
Yxmaxy <vrecer.marko@gmail.com>
2022-12-15 20:21:20 +0100

    Simplified building

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: "C:\\Users\\Marko\\Documents\\GitHub\\coordiProd",
+    outDir: "./dist",
     emptyOutDir: true,
   },
   base: "/",
