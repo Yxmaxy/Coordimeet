df62c4c9e9bb396cbfbd1ad67d2e2e7a6704f918
Yxmaxy <vrecer.marko@gmail.com>
2022-12-15 12:40:10 +0100

    Prepared new pages

--- frontend/src/common/router.ts
+++ frontend/src/common/router.ts
@@ -1,15 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import ExamplePage from "../pages/ExamplePage.vue";
+import HomePage from "../pages/HomePage.vue";
 import EventPage from "../pages/EventPage.vue";
 import EventCreatePage from "../pages/EventCreatePage.vue";
+import EventListPage from "../pages/EventListPage.vue";
+import EventAttendPage from "../pages/EventAttendPage.vue";
 
 // Register all appliaction paths here
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/", component: ExamplePage },
+        { path: "/", component: HomePage },
         { path: "/event", component: EventPage },
         { path: "/event/new", component: EventCreatePage },
+        { path: "/event/list", component: EventListPage },
+        { path: "/event/attend", component: EventAttendPage },
+        { path: "/:catchAll(.*)", redirect: () => "" },
     ]
 });
---
+++ frontend/src/pages/EventAttend.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        here user will select if they are attending the event or not
+    </div>
+</template>
