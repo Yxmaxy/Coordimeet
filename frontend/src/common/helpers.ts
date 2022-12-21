5411c142ed6c53b85d4c6784945a6ed96b0b3b4a
Yxmaxy <vrecer.marko@gmail.com>
2022-12-21 11:28:58 +0100

    Added deadline

--- frontend/src/common/helpers.ts
+++ frontend/src/common/helpers.ts
@@ -1,12 +1,25 @@
+import { CalendarType } from "./interfaces";
+
+function padNumber(number: number, places: number = 2): string {
+    return number.toString().padStart(places, '0');
+}
+
 export function formatDateDayMonth(date: Date) {
     return `${date.getDate()}. ${date.getMonth() + 1}.`;
 }
 
 export function formatDateHour(date: Date) {
-    return `${date.getHours().toString().padStart(2, '0')}:00`;
+    return `${padNumber(date.getHours())}:00`;
 }
 
 export function removeHoursMinutesFromDate(date: Date): Date {
     date.setHours(0, 0);
     return date;
+}
+
+export function initializeDateInput(type: CalendarType): string {
+    const now = removeHoursMinutesFromDate(new Date());
+    if (type === CalendarType.Date)
+        return `${now.getFullYear()}-${padNumber(now.getMonth() + 1)}-${padNumber(now.getDate())}`;
+    return `${now.getFullYear()}-${padNumber(now.getMonth() + 1)}-${padNumber(now.getDate())}T${padNumber(now.getHours())}:00`;
 }
