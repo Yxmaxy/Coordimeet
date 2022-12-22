7a88724a3a79f54471a35a32f5712f1b048071e9
Yxmaxy <vrecer.marko@gmail.com>
2022-12-23 00:14:01 +0100

    Changed and added functionality to eventUser.php API endpoint to support /event/list operations. Added data presentation for /event/list view

--- frontend/src/pages/EventListpage.vue
+++ frontend/src/pages/EventListpage.vue
@@ -1,81 +1,134 @@
 <template>
-    <div class="event-list-page">
-        <div>
-            <h1>Events I'm invited to</h1>
-            <div>
-                <div class="event" v-for="x in 100">
-                    <div><b>Name</b></div>
-                    <div>
-                        <div>Deadline</div>
-                        <div>Organiser</div>
-                    </div>
-                </div>
-            </div>
+  <div class="event-list-page">
+    <div>
+      <h1>Events I'm invited to</h1>
+      <div>
+        <div class="event" v-for="event in eventsInvited">
+          <div><b>{{ event.Name }}</b></div>
+          <div>
+            <div>{{ formatDateDayMonthYear(new Date(event.Deadline)) }}</div>
+            <div>{{ event.Organizer?.FirstName }} {{ event.Organizer?.LastName }}</div>
+          </div>
         </div>
-        <div>
-            <h1>
-                <div>Events I've created</div>
-                <button @click="$router.push('/event/new')">New event</button>
-            </h1>
-        </div>
+      </div>
     </div>
+    <div>
+      <h1>
+        <div>Events I've created</div>
+        <button @click="$router.push('/event/new')">New event</button>
+      </h1>
+      <div>
+        <div class="event" v-for="event in eventsCreated">
+          <div><b>{{ event.Name }}</b></div>
+          <div>
+            <div>{{ formatDateDayMonthYear(new Date(event.Deadline)) }}</div>
+            <div>{{ event.Organizer?.FirstName }} {{ event.Organizer?.LastName }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import {IEvent} from "../common/interfaces";
+import {formatDateDayMonthYear} from '../common/helpers';
+import axios from "axios";
+import {apiServer} from "../common/globals";
+
 export default {
-    data() {
-        return {
-            events: []  // events from api
-        }
+  data() {
+    return {
+      eventsInvited: [] as IEvent[],
+      eventsCreated: [] as IEvent[]
     }
+  },
+  methods: {
+    getEventsInvited() {
+      axios.get(`${apiServer}/eventUser.php`, {
+        params: {
+          IDUser: 1234, // TODO: Change static user id to GoogleID of logged in user (returned in googleLogin.php call)
+        }
+      }).then(res => {
+        if (res.data.error) {
+          alert(`Pri pridobivanju podatkov je prišlo do napake: ${res.data.error}`)
+          this.$router.push("/");
+          return;
+        }
+        this.eventsInvited = {...res.data} as IEvent[]
+      });
+    },
+    getEventsCreated() {
+      axios.get(`${apiServer}/eventUser.php`, {
+        params: {
+          IDOrganizer: 1234, // TODO: Change static organizer id to GoogleID of logged in user (returned in googleLogin.php call)
+        }
+      }).then(res => {
+        if (res.data.error) {
+          alert(`Pri pridobivanju podatkov je prišlo do napake: ${res.data.error}`)
+          this.$router.push("/");
+          return;
+        }
+        this.eventsCreated = {...res.data} as IEvent[]
+      });
+    },
+    formatDateDayMonthYear
+  },
+  mounted() {
+    this.getEventsInvited();
+    this.getEventsCreated();
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../styles/colors.scss";
+
 .event-list-page {
-    $sectionPadding: 1rem;
+  $sectionPadding: 1rem;
 
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 
-    & > div {
-        overflow: auto;
-        &:first-child {
-            border-right: 2px solid $color-top-bottom;
-        }
-        h1 {
-            position: sticky;
-            top: 0;
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            z-index: 10;
-            
-            background-color: $color-background-3;
-            padding: $sectionPadding;
-        }
+  & > div {
+    overflow: auto;
 
-        .event {
-            background-color: $color-background-3;
-            margin: 0.75rem;
-            padding: 1.25rem;
-            border-radius: 20px;
-            display: flex;
-            justify-content: space-between;
-            cursor: pointer;
-            transition: 200ms;
-
-            &:hover {
-                background-color: $color-top-bottom;
-                transform: translateY(-0.1rem);
-            }
-
-            & > div {
-                display: flex;
-                gap: 0.75rem;
-            }
-        }
+    &:first-child {
+      border-right: 2px solid $color-top-bottom;
     }
+
+    h1 {
+      position: sticky;
+      top: 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      z-index: 10;
+
+      background-color: $color-background-3;
+      padding: $sectionPadding;
+    }
+
+    .event {
+      background-color: $color-background-3;
+      margin: 0.75rem;
+      padding: 1.25rem;
+      border-radius: 20px;
+      display: flex;
+      justify-content: space-between;
+      cursor: pointer;
+      transition: 200ms;
+
+      &:hover {
+        background-color: $color-top-bottom;
+        transform: translateY(-0.1rem);
+      }
+
+      & > div {
+        display: flex;
+        gap: 0.75rem;
+      }
+    }
+  }
 }
 </style>
