b7248b7854ac87ce41297984deb0a484c6ba8abd
Yxmaxy <vrecer.marko@gmail.com>
2022-12-28 20:01:00 +0100

    eventDate.php Fix for EventDates (endDate has been shortened for one hour/day). EndDate now means last hour/day that is still spent on the event.
    {
        "StartDate": "2022-12-31 18:00:00",
        "EndDate": "2022-12-31 23:00:00"
    }
    
    This now means that event starts at 2022-12-31 18:00:00 and ends at 2023-01-01 00:00:00, because 2022-12-31 23:00:00 is the last hour spent on event.

--- frontend/src/common/globals.ts
+++ frontend/src/common/globals.ts
@@ -1,2 +1,3 @@
-export const developmentMode = process.env.NODE_ENV === "development";
+//export const developmentMode = process.env.NODE_ENV === "development";
+export const developmentMode = true;
 export const apiServer = "https://coordimeet.eu/backend";
