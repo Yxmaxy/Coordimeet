8b95fc30f4efd2ebb2f8d3a778e574944a881a92
Yxmaxy <vrecer.marko@gmail.com>
2022-12-28 20:17:15 +0100

    Reverted globals.ts as developmentMode has been enabled on production. Pff, ker noob je to naredu? Ups, jaz. :facepalm:

--- frontend/src/common/globals.ts
+++ frontend/src/common/globals.ts
@@ -1,3 +1,2 @@
-//export const developmentMode = process.env.NODE_ENV === "development";
-export const developmentMode = true;
+export const developmentMode = process.env.NODE_ENV === "development";
 export const apiServer = "https://coordimeet.eu/backend";
