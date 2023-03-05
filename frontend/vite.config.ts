e7446af38838d6952e8ab4b8c0f20400820a97ae
Yxmaxy <vrecer.marko@gmail.com>
2023-03-05 15:14:58 +0100

    Progress for frontend app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: { alias: { '@': '/src' } },
   plugins: [vue()],
   build: {
     outDir: "./dist",
     emptyOutDir: true,
   },
   base: "/",
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 3000,
+  }
 })
