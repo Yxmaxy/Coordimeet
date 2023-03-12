4b8de60d41b0fd21e68b4b3a7473e9cc536a32de
Yxmaxy <vrecer.marko@gmail.com>
2023-03-12 20:21:24 +0100

    Progress

--- frontend/src/common/globals.ts
+++ frontend/src/common/globals.ts
@@ -1,3 +1,3 @@
 export const developmentMode = process.env.NODE_ENV === "development";
-export const apiServer = "https://coordimeet.eu/backend";
-export const eventServer = "https://coordimeet.eu/#/event";
+export const apiServer = developmentMode ? "127.0.0.1:8080" : "https://coordimeet.eu/backend";
+export const eventServer = developmentMode ? "127.0.0.1:3000/#/event" : "https://coordimeet.eu/#/event";
