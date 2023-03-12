e1a0cea8941b289e210823745eb21dc3e930d8a4
Yxmaxy <vrecer.marko@gmail.com>
2023-03-12 20:55:34 +0100

    Added documentation, fixed some errors

--- frontend/src/common/globals.ts
+++ frontend/src/common/globals.ts
@@ -1,3 +1,3 @@
 export const developmentMode = process.env.NODE_ENV === "development";
-export const apiServer = developmentMode ? "127.0.0.1:8080" : "https://coordimeet.eu/backend";
-export const eventServer = developmentMode ? "127.0.0.1:3000/#/event" : "https://coordimeet.eu/#/event";
+export const apiServer = developmentMode ? "http://127.0.0.1:8080" : "https://coordimeet.eu/backend";
+export const eventServer = developmentMode ? "http://127.0.0.1:3000/#/event" : "https://coordimeet.eu/#/event";
