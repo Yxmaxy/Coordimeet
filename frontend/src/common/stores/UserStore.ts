5c29ec765f8c67d584d12b09f3866d6dfef3ac99
Yxmaxy <vrecer.marko@gmail.com>
2023-05-03 20:22:33 +0200

    Fixes

--- frontend/src/common/stores/UserStore.ts
+++ frontend/src/common/stores/UserStore.ts
@@ -1,26 +1,14 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
-import { apiServer, developmentMode } from '../globals';
+import { apiServer } from '../globals';
 import { IUser } from '../interfaces';
 
 export const useUserStore = defineStore("UserStore", {
     state: () => {
-        if (developmentMode)
-            return {
-                isLoggedIn: true,
-                user: {
-                    Email: "vrecer.marko@gmail.com",
-                    FirstName: "Marko",
-                    GoogleID: "110672583056965135304",
-                    LastName: "Vrečer",
-                    ProfilePhoto: "https://lh3.googleusercontent.com/a/AEdFTp6jLmYJg8rLt26Dm2F9SbDq0tc2HI_qTZwA2knwYw=s96-c"
-                } as IUser,
-            }
-        else
-            return {
-                isLoggedIn: false,
-                user: {} as IUser,
-            }
+        return {
+            isLoggedIn: false,
+            user: {} as IUser,
+        }
     },
     actions: {
         async loginUser(): Promise<boolean> {
