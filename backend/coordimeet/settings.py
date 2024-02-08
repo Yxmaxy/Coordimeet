182cad677b389c7bd24777ee80fdf277cc6a08cb
Yxmaxy <vrecer.marko@gmail.com>
2024-02-08 20:26:17 +0100

    Started apps

--- backend/coordimeet/settings.py
+++ backend/coordimeet/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external apps
+    "rest_framework",
+
+    # internal apps
+    "apps.users",
+    "apps.utils",
+    "apps.events",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +133,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
